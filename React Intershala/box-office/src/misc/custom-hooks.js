8a14d454abb694d7e63618484dd6215692d43414
yugender567 <yugenderyogi2711@gmail.com>
2021-07-03 21:21:47 +0530

    created custom hooks for shows

---
+++ React Intershala/box-office/src/misc/custom-hooks.js
@@ -0,0 +1,33 @@
+import { useReducer, useEffect } from 'react';
+
+function showsReducer(prevState, action) {
+  switch (action.type) {
+    case 'ADD': {
+      return [...prevState, action.ShowId];
+    }
+    case 'REMOVE': {
+      return prevState.filter(showId => showId !== action.showId);
+    }
+
+    default:
+      return prevState;
+  }
+}
+
+function usePersistedReducer(reducer, initailState, key) {
+  const [state, dispatch] = useReducer(reducer, initailState, initail => {
+    const persisted = localStorage.getItem(key);
+
+    return persisted ? JSON.parse(persisted) : initail;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state, key]);
+
+  return [state, dispatch];
+}
+
+export function useshows(key = 'shows') {
+  return usePersistedReducer(showsReducer, [], key);
+}
