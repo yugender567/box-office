6722f3685cf51586b1d9a627a67643369c0a9760
yugender567 <yugenderyogi2711@gmail.com>
2021-07-04 13:35:47 +0530

    finishing Starred page

--- React Intershala/box-office/src/pages/Starred.js
+++ React Intershala/box-office/src/pages/Starred.js
@@ -1,8 +1,43 @@
-import React from 'react';
+/* eslint-disable no-console */
+import React, { useEffect, useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
+import { useShows } from '../misc/custom-hooks';
+import ShowGrid from '../components/show/ShowGrid';
 
 const Starred = () => {
-  return <MainPageLayout>This is starred</MainPageLayout>;
+  const [starred] = useShows();
+  const [shows, setShows] = useState(null);
+  const [isLoading, setisLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (starred && starred.length) {
+      const promises = starred.map(showId => apiGet(`/shows/${showId}`));
+
+      Promise.all(promises)
+        .then(apiData => apiData.map(show => ({ show })))
+        .then(results => {
+          setShows(results);
+          setisLoading(false);
+        })
+        .catch(err => {
+          setError(err.message);
+          setisLoading(false);
+        });
+    } else {
+      setisLoading(false);
+    }
+  }, [starred]);
+
+  return (
+    <MainPageLayout>
+      {isLoading && <div>shows are still loading </div>}
+      {error && <div>Error occurred : {error}</div>}
+      {!isLoading && !shows && <div>No shows are added</div>}
+      {!isLoading && !error && shows && <ShowGrid data={shows} />}
+    </MainPageLayout>
+  );
 };
 
 export default Starred;
