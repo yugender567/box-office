f843a70cf0d9f0645e56dff63cc20c444a559e11
yugender567 <yugenderyogi2711@gmail.com>
2021-07-05 22:48:06 +0530

    Updates

---
+++ precache-manifest.f6a94bad0ffd57654a79ead5fba8e0f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62fabb3cbf2f5392717732c7445c9d8c",
+    "url": "/box-office/index.html"
+  },
+  {
+    "revision": "bd1779166d67f4416850",
+    "url": "/box-office/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "4886eec084435b0c5098",
+    "url": "/box-office/static/js/2.17fc357b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/box-office/static/js/2.17fc357b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd1779166d67f4416850",
+    "url": "/box-office/static/js/main.f5ff07e4.chunk.js"
+  },
+  {
+    "revision": "a24731daa79adae5d2cf",
+    "url": "/box-office/static/js/runtime-main.796231bd.js"
+  }
+]);
